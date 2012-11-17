8da62fe573a7d466ee5a7385b7cddd62bcf09c59
James Lewis <kharmajbird@gmail.com>
2012-11-17 11:00:48 -0800

    a handy start

--- genpuppet.pl
+++ genpuppet.pl
@@ -1,1 +1,47 @@
 #!/usr/bin/perl
+
+my %resources;
+my @requires, @includes;
+my $manifest, $classname;
+my $var, $include=0, $nullmailer=0, $semaphore=0;
+
+
+
+print "Name of manifest? ";
+chomp ($manifest = <>);
+
+print "Name of class? ";
+chomp ($classname = <>);
+
+
+# get as many top-level requires and includes as necessary
+#
+print "Top level require? ";
+chomp ($var = <>);
+
+while ($var) {
+    push (@requires, $var);
+    print "Top level require? ";
+    chomp ($var = <>);
+}
+
+print "Top level include? ";
+chomp ($var = <>);
+
+while ($var) {
+    push (@include, $var);
+    print "Top level include? ";
+    chomp ($var = <>);
+}
+
+
+# Use class nullmailer or exec set-semaphore?
+#
+print "Use class nullmailer? [no] ";
+chomp ($var = <>);
+if ($var) { $nullmailer=1; }
+
+print "Use exec set-semaphore? [no] ";
+chomp ($var = <>);
+if ($var) { $semaphore=1; }
+
